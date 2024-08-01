de6bf2501743ba0af8bd0da5cd8eea9ac4622712
Ibrohim Jumayev <ibrohimjumayev@MacBook-Air-Ibrohim.local>
2024-08-01 09:18:42 +0500

    active link style added

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import path from "path";
 
 export default function Nav() {
+  const pathname = usePathname();
   return (
     <div className="max-w-7xl m-auto flex justify-between mt-9 items-center">
       <div>
@@ -12,16 +17,48 @@ export default function Nav() {
       <div className="flex items-center gap-7">
         <ul className="flex items-center gap-12 font-bold">
           <Link href="/">
-            <li>Home</li>
+            <li
+              className={`${
+                pathname == "/"
+                  ? "text-buttonColor font-bold scale-110 duration-300"
+                  : ""
+              }`}
+            >
+              Home
+            </li>
           </Link>
           <Link href="/about">
-            <li>About</li>
+            <li
+              className={`${
+                pathname == "/about"
+                  ? "text-buttonColor font-bold scale-110 duration-300"
+                  : ""
+              }`}
+            >
+              About
+            </li>
           </Link>
           <Link href="/services">
-            <li>Services</li>
+            <li
+              className={`${
+                pathname == "/services"
+                  ? "text-buttonColor font-bold scale-110 duration-300"
+                  : ""
+              }`}
+            >
+              Services
+            </li>
           </Link>
           <Link href="/system">
-            <li>Systems</li>
+            <li
+              className={`${
+                pathname == "/system"
+                  ? "text-buttonColor font-bold scale-110 duration-300"
+                  : ""
+              }`}
+            >
+              Systems
+            </li>
           </Link>
         </ul>
         <button className="text-buttonColor font-boldd border-2 px-4 py-4 rounded-full border-buttonColor hover:bg-buttonColor hover:text-white duration-500">
